d08f4f497647463dee209b11bac9bd094fb8df13
Quentin MARQUES <marques@ece.fr>
2012-10-22 17:27:36 +0200

    LAB Report
    Template document

---
+++ PHP/home.php
@@ -0,0 +1,29 @@
+﻿<!DOCTYPE html>
+<html>
+	<!-- Chargement du <head> -->
+    <!-- <?php include("head.php") ?>
+    -->
+    <body>
+	<div id="page">
+        
+        <!-- Chargement de l'en-tête de la page (incluant bandeau de login) -->
+		<!-- <?php include("header.php") ?>
+        -->
+        <!-- Chargement du panneau de navigation -->
+        <!-- <?php include("nav.php") ?>
+        -->
+        
+        <section>
+            <article>
+            
+                
+            
+            </article>
+        </section>
+        
+        <!-- Chargement du pied de page (lien contact, twitter etc...)-->
+        <!-- <?php include("footer.php") ?>
+		-->
+        </div>
+    </body>
+</html>
