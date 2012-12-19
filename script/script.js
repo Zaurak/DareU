83a5ceef4afe8718be933fd5542a6c7cff25ced0
Gaetan Fleury <fleury@ece.fr>
2012-12-19 22:39:49 +0100

    Essais avec javascript. Ne fonctionne pas, a revoir.

--- script/script.js
+++ script/script.js
@@ -2,7 +2,49 @@ function validateSignInForm() {
 	// TODO
 }
 
+
 function validateSignUpForm() {
-	// TODO	
+	alert('Pouet');
+	return (checkName() && checkEmail() && checkPassword());
+}
+
+function checkName() {
+	var name = document.getElementById('username');
+
+	if(nickname.value == '' || nickname.value.length <= 4) {
+		// Nickname is required and must be at least 4 characters long
+		nickname.style.color = 'red';
+		return false;
+	}
+	else {
+		nickname.style.color = 'black';
+		return true;
+	}
+}
+
+function checkEmail() {
+	var email = document.getElementById('email');
+	var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	
+	if(!filter.test(email.value) || email.value == '') {
+		email.style.color = 'red';
+		return false;
+	}
+	else {
+		email.style.color = 'black';
+		return true;
+	}
+}
+
+function checkPassword() {
+	var password = document.getElementById('password');
+	if(password.value == '' || password.value.length < 4) {
+		password.style.color = 'red';
+		return false;
+	}
+	else {
+		password.style.color = 'black';
+		return true;
+	}
 }
 
